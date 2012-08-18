6cec6a4f0519c843df3392604db248d25f095573
Prasanna N <prasann87@gmail.com>
2012-08-18 22:42:28 +0530

    graph matrix is ready

--- lib/tourist/graph_matrix.rb
+++ lib/tourist/graph_matrix.rb
@@ -1,3 +1,21 @@
 class GraphMatrix
   attr_accessor :matrix
-end
+  
+  def initialize
+    @matrix = {}
+  end
+
+  def set(v1, v2, weight)
+    vertices = [v1, v2]
+    @matrix[vertices] = weight
+  end
+
+  def get(v1, v2)
+    matrix.fetch [v1, v2]
+  rescue KeyError
+    raise NoRouteError, "No route found between #{v1} and #{v2}!"
+  end
+
+end # class GraphMatrix
+
+class NoRouteError < StandardError; end
--- spec/tourist/graph_matrix_spec.rb
+++ spec/tourist/graph_matrix_spec.rb
@@ -2,6 +2,36 @@ require_relative '../spec_helper'
 require_relative '../../lib/tourist/graph_matrix'
 
 describe GraphMatrix do
-   it "has the route matrix attribute" do
-   end
-end
+  before do
+    @it = GraphMatrix.new
+    @v1, @v2 = :a, :b
+    @weight = 12
+    @vertices = [@v1, @v2]
+  end
+
+  it "has the matrix attribute" do
+    @it.matrix.class.must_equal Hash
+  end
+
+  describe "#set" do
+    it "makes an entry for the given two vertices with its weight" do
+      ->{ @it.matrix.fetch(@vertices) }.must_raise KeyError
+      @it.set(@v1, @v2, @weight)
+      @it.matrix.fetch(@vertices).must_equal 12
+    end
+  end # describe #set
+
+  describe "#get" do
+    it "gets the weight for the given vertices, if present already" do
+      @it.set(@v1, @v2, @weight)
+      @it.get(@v1, @v2).must_equal @weight
+    end
+
+    it "fails for vertices with no entry in the matrix" do
+      @it.matrix.size.must_equal 0
+      @it.set(@v1, @v2, @weight)
+      @it.matrix.size.must_equal 1
+      ->{ @it.get(@v1, :c) }.must_raise NoRouteError
+    end
+  end # describe #get
+end # describe GraphMatrix
