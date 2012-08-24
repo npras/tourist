c89b876d702cf3c30e367705e7a643f0eb6f6bc8
Prasanna N <prasann87@gmail.com>
2012-08-24 13:52:12 +0530

    dumn

--- lib/tourist.rb
+++ lib/tourist.rb
@@ -1,3 +1,4 @@
+require 'debugger'
 require_relative "tourist/graph"
 
 module Tourist
@@ -8,6 +9,7 @@ module Tourist
     
     def initialize
       @graph = Tourist::Graph.new
+      graphify
     end
 
     def graphify
@@ -17,9 +19,42 @@ module Tourist
       end
     end
 
+    #def dijkstra(source)
+      #dist = previous = {}
+      #graph.vertices.each do |v| # initializations
+        #dist[v] = 1.0/0.0 # infinity
+        #previous[v] = 1.0/0.0
+      #end
+      #dist[source] = 0 # distance to self is zero
+      #q = graph.vertices
+      #until q.empty? # main loop
+        #u = dist.min[0]
+        #q.delete u
+        #break if dist[u] == 1.0/0.0
+        ##graph.neighbors(u).each do |v|
+        #for v in graph.neighbors(u)
+          #debugger
+          #alt = dist[u] + graph.get_edge_values(u, v).first.to_i
+          #p "alt", alt
+          #p "dist[v]"
+          #p dist[v]
+          #if alt < dist[v]
+            #dist[v] = alt
+            #previous[v] = u
+            ## missing_step
+          #end # if
+        #end # for
+      #end # until
+      #dist
+    #end
+
+    def my_dijkstra(source)
+
+    end
+
   end # class Run
 end # module Tourist
 
 if __FILE__ == $0
-  Tourist::Run.new.graphify
+  p Tourist::Run.new.dijkstra 'A'
 end
